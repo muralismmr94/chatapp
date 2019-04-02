fd29a18c235ce3d8add56400e1913338829ebe32
admin <you@example.com>
2019-04-02 09:05:28 +0530

    chatapplication

--- client/app.js
+++ client/app.js
@@ -1,3 +1,13 @@
+/***************************************************************************
+ * Execution : default node     cmd>live-server
+ * purpose  : chatting two peoples
+ * @description
+ * @file    : app.js
+ * @overview :In this module contains the sending and receiving messages.
+ * @author  : Murali s <muralismmr94@gmail.com>
+ * @version :1.0
+ * 
+ */
 var app = angular.module('chatapp', ['ui.router','btford.socket-io']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
